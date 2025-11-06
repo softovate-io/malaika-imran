2fc9b750acf441d714289df3ce0178add22c9ec0
b127exe <bilalimran6217523@gmail.com>
2025-11-07 00:18:08 +0500

    commit removed

--- api/contentful-proxy.js
+++ api/contentful-proxy.js
@@ -1,27 +1,3 @@
-// export default async function handler(req, res) {
-//   const { spaceId, contentType } = req.query;
-
-//   if (!spaceId || !contentType) {
-//     return res.status(400).json({ message: "Missing required parameters" });
-//   }
-
-//   const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/master/entries?content_type=${contentType}`;
-
-//   try {
-//     const response = await fetch(url, {
-//       headers: {
-//         Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-//       },
-//     });
-
-//     const data = await response.json();
-//     res.status(200).json(data);
-//   } catch (error) {
-//     console.error("Error fetching from Contentful:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// }
-
 export default async function handler(req, res) {
   const { spaceId, contentType } = req.query;
 
